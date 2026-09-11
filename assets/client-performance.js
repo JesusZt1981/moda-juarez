e0847c402c4cf6b7baa07727813419d28bc05c16
JesusZt1981 <jzutenorio@gmail.com>
2026-09-10 18:53:39 -0600

    Forzar versión nueva del checkout de pago

--- assets/client-performance.js
+++ assets/client-performance.js
@@ -34,7 +34,7 @@
   function loadCheckoutPayment(){
     if(document.querySelector('script[data-w656-checkout-payment]'))return;
     const s=document.createElement('script');
-    s.src='assets/checkout-payment.js?v=1';
+    s.src='assets/checkout-payment.js?v=2';
     s.async=false;
     s.dataset.w656CheckoutPayment='1';
     document.head.appendChild(s);
